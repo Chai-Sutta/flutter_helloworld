d5b8fd73110fe6d13b9c47544585fc0d7d89e550
Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>
2020-04-27 17:31:43 +0530

    remove Debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Random Word Generator',
         theme: ThemeData(
           primaryColor: Colors.grey[900],
